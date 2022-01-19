93cefa25ac9c97a81539a22a1ed2cf162a483df5
21pages <pages21@163.com>
2022-01-19 21:32:32 +0800

    baisc: generic
    Signed-off-by: 21pages <pages21@163.com>

---
+++ basic/generic/src/test/mod.rs
@@ -0,0 +1,12 @@
+mod test_bound;
+mod test_bound2;
+mod test_multi_bound;
+
+mod test_about_type;
+mod test_function;
+mod test_impl;
+mod test_new_type;
+mod test_trait;
+mod test_unit_check;
+mod test_virtual_type;
+mod test_where;
