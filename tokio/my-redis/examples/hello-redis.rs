3b6b460cf5ae2dd51c12560412673e4bbf1e27ad
21pages <pages21@163.com>
2022-01-27 10:02:04 +0800

    tokio: myredis examples hello-redis
    Signed-off-by: 21pages <pages21@163.com>

---
+++ tokio/my-redis/examples/hello-redis.rs
@@ -0,0 +1,14 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:1234").await?;
+    let kv = ("hello", "world");
+    client.set(kv.0,  kv.1.into()).await?;
+    let result = client.get(kv.0).await?.unwrap();
+    println!("get val from {:?}", result);
+
+    Ok(())
+}
+
+
