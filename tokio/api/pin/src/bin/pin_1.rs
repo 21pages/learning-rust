50c4ed69076b95ab8b337684fb28787abbf030a6
21pages <pages21@163.com>
2022-01-28 21:53:42 +0800

    tokio: api pin
    Signed-off-by: 21pages <pages21@163.com>

---
+++ tokio/api/pin/src/bin/pin_1.rs
@@ -0,0 +1,13 @@
+use tokio::pin;
+
+async fn my_async_fn() {
+    print!("hello");
+}
+
+#[tokio::main]
+async fn main() {
+    let future = my_async_fn();
+    pin!(future);
+
+    (&mut future).await;
+}
